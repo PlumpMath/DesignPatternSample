87ad6882059b11ea84512731397b74742a913f74
Kei Tashima <tassi.yuzukko@gmail.com>
2016-11-22 23:29:51 +0900

    もっとPrototypeパターンがわかりやすいようにしてみた

--- 06‗Prototype/Program.cs
+++ 06‗Prototype/Program.cs
@@ -9,26 +9,27 @@ namespace Prototype
 {
     class Program
     {
+        const string hello = "Hello, world !!";
+
         static void Main(string[] args)
         {
             // 準備
             var manager = new Manager();
-            IProduct upen = new UnderlinePen('~');
-            IProduct mbox = new MessageBox('*');
-            IProduct sbox = new MessageBox('/');
 
-            manager.Register("strong message", upen);
-            manager.Register("warning box", mbox);
-            manager.Register("slash box", sbox);
+            // 登録
+            manager.Register("strong message", new UnderlinePen('~'));
+            manager.Register("warning box", new MessageBox('*'));
+            manager.Register("slash box", new MessageBox('/'));
 
             // 生成
-            const string hello = "Hello, world !!";
             IProduct p1 = manager.Create("strong message");
             IProduct p2 = manager.Create("warning box");
             IProduct p3 = manager.Create("slash box");
+            IProduct p4 = manager.Create("slash box");
             p1.Use(hello);
             p2.Use(hello);
             p3.Use(hello);
+            p4.Use(hello);
         }
     }
 }
