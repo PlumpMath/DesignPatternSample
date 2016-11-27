f67b8616ae0c3e52bb73be732668957d9d0a6497
Kei Tashima <tassi.yuzukko@gmail.com>
2016-11-27 10:16:06 +0900

    AbstractFactoryパターンを追加しようとしたが、めんどくさいので途中

---
+++ 08‗AbstractFactory/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _08_AbstractFactory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //描画先とするImageオブジェクトを作成する
+            Bitmap canvas = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            //ImageオブジェクトのGraphicsオブジェクトを作成する
+            Graphics g = Graphics.FromImage(canvas);
+
+            //(10, 20)-(100, 200)に、幅1の黒い線を引く
+            g.DrawLine(Pens.Black, 10, 20, 100, 200);
+
+            //リソースを解放する
+            g.Dispose();
+            //PictureBox1に表示する
+            pictureBox1.Image = canvas;
+        }
+    }
+}
